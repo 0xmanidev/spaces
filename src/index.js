9364b2a514914302e71f9b88e9bc8a562c23bd43
Eternal <73596193+eofreternal@users.noreply.github.com>
2026-06-18 17:49:11 -0400

    Revert "uhhh. will migrations work now?"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import app from './app.js';
-import pg from './utils/db.js';
 import { startAutoStopJob } from './utils/auto-stop.js';
 
 const port = process.env.PORT || 5678;
 
-async function main() {
-	await pg.migrate.latest({
-		directory: "./migrations"
-	})
+app.listen(port, () => {
+	console.log(`Server is up at port http://localhost:${port}`);
+	startAutoStopJob();
+});
 
-	app.listen(port, () => {
-		console.log(`Server is up at port http://localhost:${port}`);
-		startAutoStopJob();
-	});
-}
