3e86e7d1bde6d994a42a7aa4d4e0496515393be8
Eternal <chasezou09@gmail.com>
2026-06-18 18:18:25 -0400

    Revert "uhhh. Let's try this migration thing again, shall we?"
    This reverts commit cbf229b728ab5e900bf66976b269468cda81bab7.

--- migrations/20260515214034_add_vscode_extension_to_users.js
+++ migrations/20260515214034_add_vscode_extension_to_users.js
@@ -1,11 +1,7 @@
 export async function up(knex) {
-    const hasColumn = await knex.schema.hasColumn('users', 'vscode_extensions');
-
-    if (!hasColumn) {
-        await knex.schema.alterTable("users", (table) => {
-            table.text('vscode_extensions');
-        });
-    }
+    await knex.schema.alterTable("users", (table) => {
+        table.text('vscode_extensions');
+    });
 };
 
 export async function down(knex) {
